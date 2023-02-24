945a3c6ccaf5c913dc38332c7dd3e8ec55413b37
cab557 <102686043+cab557@users.noreply.github.com>
2023-02-24 15:04:13 -0500

    Made Main script
    adjusted enemy and bound script

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -1,7 +1,10 @@
+using OpenCover.Framework.Model;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
+[RequireComponent(typeof(BoundsCheck))]
 public class Enemy : MonoBehaviour
 {
     [Header("Inscribed")]
@@ -9,9 +12,14 @@ public class Enemy : MonoBehaviour
      public float fireRate = 0.3f;  // Seconds/shot (Unused)
      public float health = 10;    // Damage needed to destroy this enemy
      public int score = 100;   // Points earned for destroying this
- 
-     // This is a Property: A method that acts like a field
-     public Vector3 Pos
+    private BoundsCheck bndCheck;
+
+    void Awake()
+    {                                                            // c
+        bndCheck = GetComponent<BoundsCheck>();
+     }
+    // This is a Property: A method that acts like a field
+    public Vector3 Pos
     {                                                       // a
          get
          {
@@ -22,12 +30,27 @@ public class Enemy : MonoBehaviour
          this.transform.position = value;
          }
      }
- 
-     void Update()
-     {
+
+    void Update()
+    {
         Move();                                                                // b
-     }
- 
+       
+        if (bndCheck.LocIs(BoundsCheck.eScreenLocs.offDown))
+        {             // a
+         Destroy(gameObject);
+        }
+        // Check whether this Enemy has gone off the bottom of the screen
+           // if (!bndCheck.isOnScreen)
+       // {
+            // d
+       //     if (Pos.y < bndCheck.camHeight - bndCheck.radius)
+         //   {
+                // We’re off the bottom, so destroy this GameObject
+       //         Destroy(gameObject);
+         //   }
+        }
+    
+
      public virtual void Move()
      { // c
         Vector3 tempPos = Pos;
